8389cb7f26a015db629c2e56d3bbd0c9f45b8983
Aadharsh Nuckhir <aadharshnuckhir@gmail.com>
2023-11-19 15:02:10 +0400

    Role changed.

--- Dev/Lambda/uc08/lambda.tf
+++ Dev/Lambda/uc08/lambda.tf
@@ -1,7 +1,7 @@
 ######Create a lambda function for presigned
 resource "aws_lambda_function" "presigned_url" {
   function_name = "presigned-url"
-  role          =  "arn:aws:iam::496051641293:role/aadharsh-role"
+  role          =  "arn:aws:iam::496051641293:role/AadharshInstanceProfile"
   image_uri     = "496051641293.dkr.ecr.eu-central-1.amazonaws.com/presigned-url:latest"
   timeout       = 30 # seconds
   package_type  = "Image"
